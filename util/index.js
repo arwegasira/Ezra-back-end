730238115c6fa54840852260244e2235a0d5defa
arwegasira <ac.rwegasira@gmail.com>
2024-02-18 20:04:00 +0200

    correct typo

--- util/index.js
+++ util/index.js
@@ -1,15 +1,14 @@
-
-const SendVerficationEmail = require('./sendVerificationEmail');
-const {attachCookies,isTokenValid} = require('./jwt');
+const SendVerficationEmail = require('./sendVerificationEmail')
+const { attachCookies, isTokenValid } = require('./jwt')
 const createPayload = require('./createpayload')
-const SendpasswordResetEmail = require('./sendpasswordresetemail');
-const createHash = require('./createhash');
+const SendpasswordResetEmail = require('./sendPasswordResetEmail')
+const createHash = require('./createhash')
 
-module.exports ={
-    SendVerficationEmail,
-    isTokenValid,
-    attachCookies,
-    createPayload,
-    SendpasswordResetEmail,
-    createHash
+module.exports = {
+  SendVerficationEmail,
+  isTokenValid,
+  attachCookies,
+  createPayload,
+  SendpasswordResetEmail,
+  createHash,
 }
